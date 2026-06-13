b12a38a5329c1026263e868e92ba931201d889b0
Autumnmaple1 <hongyiming000003@163.com>
2026-06-13 17:32:24 +0800

    update_bird

--- frontend/src/utils/birdAssets.ts
+++ frontend/src/utils/birdAssets.ts
@@ -2,24 +2,34 @@ import baige from '../assets/birds/白头鹎.png';
 import bage from '../assets/birds/八哥.png';
 import daShanQue from '../assets/birds/大山雀.png';
 import gezi from '../assets/birds/鸽子.png';
+import huiLiangNiao from '../assets/birds/灰椋鸟.png';
+import huiXiQue from '../assets/birds/灰喜鹊.png';
 import maque from '../assets/birds/麻雀.png';
+import yeLu from '../assets/birds/夜鹭.png';
 import xique from '../assets/birds/喜鹊.png';
 import yanzi from '../assets/birds/燕子.png';
 import wuya from '../assets/birds/乌鸦.png';
 import wudong from '../assets/birds/乌鸫.png';
 import xiuyan from '../assets/birds/绣眼鸟.png';
+import zhuJingBanJiu from '../assets/birds/珠颈斑鸠.png';
+import baiJiLing from '../assets/birds/白鹡鸰.png';
 
 const birdAssetMap = new Map<string, string>([
   ['白头鹎', baige],
   ['八哥', bage],
   ['大山雀', daShanQue],
   ['鸽子', gezi],
+  ['灰椋鸟', huiLiangNiao],
+  ['灰喜鹊', huiXiQue],
   ['麻雀', maque],
+  ['夜鹭', yeLu],
   ['喜鹊', xique],
   ['燕子', yanzi],
   ['乌鸦', wuya],
   ['乌鸫', wudong],
   ['绣眼鸟', xiuyan],
+  ['珠颈斑鸠', zhuJingBanJiu],
+  ['白鹡鸰', baiJiLing],
 ]);
 
 export function getPixelBirdAsset(name: string) {
